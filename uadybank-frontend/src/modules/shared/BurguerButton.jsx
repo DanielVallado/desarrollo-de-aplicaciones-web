b6d3ba441938c16eaf1552eb6f50f7325a473d9e
Daniel <danielvalladoorozco@gmail.com>
2023-11-13 03:02:43 -0600

    feat: Implementación header
    Implementación del componente header

---
+++ uadybank-frontend/src/modules/shared/BurguerButton.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./BurguerButton.css";
+
+export const BurguerButton = ({ onClick, isOpen }) => {
+  const buttonClasses = `burguer-icon ${isOpen ? "open" : ""}`;
+
+  return (
+    <div>
+      <div onClick={onClick} className={buttonClasses}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+};
+
+BurguerButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+};
+
+export default BurguerButton;
---
+++ uadybank-frontend/src/modules/shared/Header.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import "./Header.css";
+import defaultUserIcon from "../../assets/user-icon.svg";
+import BurguerButton from "./BurguerButton";
+
+export const Header = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
+  function getImagePath() {
+    const path = localStorage.getItem("userImagePath");
+    if (path) {
+      return path;
+    } else {
+      return defaultUserIcon;
+    }
+  }
+
+  function getUsername() {
+    const username = localStorage.getItem("username");
+    if (username) {
+      return username;
+    } else {
+      return "Usuario";
+    }
+  }
+
+  return (
+    <div className="header">
+      <div className="header__container">
+        <div className="header__logo">
+          <a href="#">
+            <img src="/uadybank-black.svg" alt="Logo UADYBank" />
+            <h1>UADYBANK</h1>
+          </a>
+        </div>
+
+        <div className="menu-icon">
+          <BurguerButton onClick={handleClick} isOpen={clicked} />
+        </div>
+
+        <div className="header__user">
+          <a href="#">
+            <div className="header__image">
+              <img src={getImagePath()} alt="" />
+            </div>
+            <p>{getUsername()}</p>
+          </a>
+        </div>
+      </div>
+
+      <nav className={`header__options ${clicked ? "open" : ""}`}>
+        <ul>
+          <li>
+            <a href="">Inicio</a>
+          </li>
+          <li>
+            <a href="">Perfil</a>
+          </li>
+          <li>
+            <a href="">Salir</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
